25320530a19d1f20df9c42cc674f440c9d210aad
Soumyq <99405991+QMiNt@users.noreply.github.com>
2024-07-13 12:38:16 +0530

    cors

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ app.use(cors(
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE'
     }
 ));
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
 
 app.use(bodyParser.json({ limit: '5mb' }));
 // app.use(bodyParser.urlencoded({ extended: true }));
