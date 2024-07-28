a1fb28688782878568416c993e4867e6abece4a1
Soumyq <158762609+devrunch@users.noreply.github.com>
2024-07-28 20:40:21 +0530

    feat: Update checkUserJwt middleware to use admin.js

--- middleware/admin.js
+++ middleware/admin.js
@@ -1,17 +1,19 @@
 const User = require('../models/admin');
 const checkUserJwt = (req, res, next) => {
-    const token = req.cookies["infojwttoken"]
-    console.log(token)
+
+    let token = req.headers.authorization;
     if (!token) {
         return res.status(401).json({ message: 'Unauthorized' ,auth:false });
     }
+
+    token = token.replace('Bearer ', '');
+
     try {
         const decodedToken = User.validateAuthToken(token);
         req.user = decodedToken;
-        console.log("Auth")
+        console.log('Admin Logged')
         next();
     } catch (error) {
-        console.log(error)
         return res.status(401).json({ error: 'Invalid token' });
     }
 };
